853f63828435baf1017cca91be6434a195af8b00
Eduardo Sebastián <eduardo@civio.es>
2017-06-14 18:28:13 +0200

    fix service name for demo deployment

--- config/deploy/demo.rb
+++ config/deploy/demo.rb
@@ -1,2 +1,5 @@
 # Default deploy_to directory is /var/www/openbudgets/
 set :deploy_to, "/var/www/openbudgets/demo"
+
+# The service to be restarted once the app has been deployed
+set :service_name, "openbudgets.demo"
