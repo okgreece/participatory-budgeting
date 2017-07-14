b211a75e49d9a8af631f8f2e75e77942dbf7a584
skarampatakis <s.karampatakis@gmail.com>
2017-07-14 12:07:11 +0300

    changed default locale to greek

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,6 @@ Bundler.require(*Rails.groups)
 module OpenBudgets
   class Application < Rails::Application
     I18n.available_locales = [:en, :es, :el, :de, :fr]
-    I18n.default_locale = :en
+    I18n.default_locale = :el
   end
 end
